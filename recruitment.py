d2e36d1d987f46c4f0d2ec4b6ccace68e6e50eb7
ShahdAllemie0 <69586588+ShahdAllemie0@users.noreply.github.com>
2020-08-17 12:41:08 +0300

    Update recruitment.py

--- recruitment.py
+++ recruitment.py
@@ -1,5 +1,33 @@
 def main():
 	#write your code here
+  skills=["Python","C++","Javascript","Juggling","Running","Eating"]
+  cv={}
+  name=input("What's your name?")
+  cv["name"]=name
+  age=input("How old are you?")
+  cv["age"]=int(age)
+  experience=input("How many years of experience do you have?")
+  cv["experience"]=int(experience)
+  cv["skills"]=[]
+  print("Skills:")
+  s=1
+  for k in skills:
+    
+    print("%d. %s"%(s,k))
+    s=s+1
+  sk1=input("Choose a skill from above by entering its number:") 
+  sk2=input("Choose another skill from above by entering its number:") 
+  cv["skills"].append(skills[(int(sk1)-1)])
+  cv["skills"].append(skills[(int(sk2)-1)])
+ 
+  if cv["age"]>=25 and cv["age"]<=40 and cv["experience"]>5 and skills[5] in cv["skills"]:
+    # for k in cv["skills"]:
+    #   if skills[5] == cv["skills"]:
+      print("You have been accepted")
+
+
+  else:
+    print("You have been rejected")  
 
 if __name__ == '__main__':
 	main()
